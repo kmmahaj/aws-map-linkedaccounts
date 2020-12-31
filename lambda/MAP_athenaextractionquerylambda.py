eb6b12585d405d449efd58f2bf3a1448b7d77212
Mahajan <kmmahaj@amazon.com>
2020-12-31 13:16:43 -0800

    working commit

--- lambda/MAP_athenaextractionquerylambda.py
+++ lambda/MAP_athenaextractionquerylambda.py
@@ -10,6 +10,7 @@ def lambda_handler(event, context):
     
     s3outputbucketname = os.environ['s3outputbucketname']
     outputfolder = os.environ['outputfolder'] + '/'
+    print("outputfolder: " + outputfolder)
    
     map_migrated_db = os.environ['map_migrated_db']
     map_migrated_table = os.environ['map_migrated_table']
@@ -29,10 +30,15 @@ def lambda_handler(event, context):
 def create_s3outputbucketfolder(s3outputbucketname, outputfolder):
 
     client = boto3.client('s3')
+    print("inside create_s3outputbucketfolder")
     response = client.list_objects_v2(
             Bucket=s3outputbucketname
     )
+    
+    print("keycount: " + str(response['KeyCount']) )
+    
     if (response['KeyCount'] == 0):
+        print("inside create folder")
         response = client.put_object(
             Bucket=s3outputbucketname, 
             Key=outputfolder
@@ -46,20 +52,21 @@ def empty_s3outputbucket(s3outputbucketname, outputfolder):
             Bucket=s3outputbucketname,
             Prefix=outputfolder
     )
+    
     if (response['KeyCount'] > 0):
         s3objects = response['Contents']
         for s3object in s3objects:
             objectkey = s3object['Key']
+            print("objectkey is: " + objectkey)
             response = client.delete_object(
                 Bucket=s3outputbucketname,
                 Key=objectkey
             )
-            objectkey = s3object['Key']
-            response = client.delete_object(
-                Bucket=s3outputbucketname,
-                Key=objectkey
-            )
-
+        response_folder = client.put_object(
+            Bucket=s3outputbucketname, 
+            Key=outputfolder
+        ) 
+    
 # Run MAP Extraction Query for linked accounts
 def run_athenaextractionquery(map_migrated_db, map_migrated_table, extraction_query_name, athena_output_location):
 
@@ -67,15 +74,19 @@ def run_athenaextractionquery(map_migrated_db, map_migrated_table, extraction_qu
     response = client.list_named_queries()
     named_query_IDs = response['NamedQueryIds']
     
+    
     for query_ID in named_query_IDs: 
+        print("named_query_id: " + str(query_ID) )
         named_query = client.get_named_query(
             NamedQueryId=query_ID
         )
         query_string = named_query['NamedQuery']['QueryString']
         query_name = named_query['NamedQuery']['Name']
+        print("query_name: " + query_name )
         
         if extraction_query_name in query_name:
             drop_query_string = 'DROP TABLE ' + map_migrated_db + '.temp_table'
+            print("inside drop_query_string: " + drop_query_string )
             executionID_drop = client.start_query_execution(
                 QueryString=drop_query_string,
                 ResultConfiguration={
@@ -85,6 +96,7 @@ def run_athenaextractionquery(map_migrated_db, map_migrated_table, extraction_qu
                     }
                 }
             )
+            print("completed drop_query_string: " + drop_query_string )
             
             executionID_create = client.start_query_execution(
                 QueryString=query_string,
@@ -95,6 +107,5 @@ def run_athenaextractionquery(map_migrated_db, map_migrated_table, extraction_qu
                     }
                 }
             )
-
-
-    
+            print("completed create_query_string: " + query_string)
+            break
--- lambda/MAP_s3outputbucketpermissionslambda.js
+++ lambda/MAP_s3outputbucketpermissionslambda.js
@@ -1,76 +1,57 @@
-const AWS = require('aws-sdk');
-const util = require('util');
+          const AWS = require('aws-sdk');
+          const util = require('util');
+          exports.handler = function(event, context, callback) {
+              
+              // If its an object delete, do nothing
+              if (event.RequestType === 'Delete') {
+              }
+              else // Its an object put
+              {
+                  // Get the source bucket from the S3 event
+                  var srcBucket = event.Records[0].s3.bucket.name;      
+                  // Object key may have spaces or unicode non-ASCII characters, decode it
+                  var srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));  
+                  // Define the object permissions, using the permissions array
+                  var params =
+                  {
+                      Bucket: srcBucket,
+                      Key: srcKey,
+                      AccessControlPolicy:
+                      {
+                          'Owner':
+                          {
+                              'DisplayName': process.env.payeraccountid.toString(),
+                              'ID': process.env.canonicalidpayer.toString()
+                          },
+                          'Grants': 
+                          [
+                              {
+                                  'Grantee': 
+                                  {
+                                      'Type': 'CanonicalUser',
+                                      'DisplayName': process.env.payeraccountid.toString(),
+                                      'ID': process.env.canonicalidpayer.toString()
+                                  },
+                                  'Permission': 'FULL_CONTROL'
+                              },
+                              {
+                                  'Grantee': {
+                                      'Type': 'CanonicalUser',
+                                      'DisplayName': process.env.linkedaccountid.toString(),
+                                      'ID': process.env.canonicalidlinked.toString()
+                                      },
+                                  'Permission': 'READ'
+                              },
+                          ]
+                      }
+                  };
 
-// Permissions for the new objects
-// Key MUST match the top level folder
-// Format: <owner account name> - <Canonical ID> - <sub account name> - <canonical ID>
-// This will give owner full permission & sub account read only permission
-
-
-// Main Loop
-exports.handler = function(event, context, callback) {
-    
-    // If its an object delete, do nothing
-    if (event.RequestType === 'Delete') {
-    }
-    else // Its an object put
-    {
-        // Get the source bucket from the S3 event
-        var srcBucket = event.Records[0].s3.bucket.name;
-        
-        // Object key may have spaces or unicode non-ASCII characters, decode it
-        var srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));  
-
-        // Gets the top level folder, which is the key for the permissions array        
-        var folderID = srcKey.split("/")[0];
-
-        var folderkey = process.env.outputfolder;
-        var permissions = {
-            folderkey: [process.env.payeraccountid, process.env.canonicalidpayer, process.env.linkedaccountid, process.env.canonicalidlinked]
-        };
-
-        // Define the object permissions, using the permissions array
-        var params =
-        {
-            Bucket: srcBucket,
-            Key: srcKey,
-            AccessControlPolicy:
-            {
-                'Owner':
-                {
-                    'DisplayName': permissions[folderID][0],
-                    'ID': permissions[folderID][1]
-                },
-                'Grants': 
-                [
-                    {
-                        'Grantee': 
-                        {
-                            'Type': 'CanonicalUser',
-                            'DisplayName': permissions[folderID][0],
-                            'ID': permissions[folderID][1]
-                        },
-                        'Permission': 'FULL_CONTROL'
-                    },
-                    {
-                        'Grantee': {
-                            'Type': 'CanonicalUser',
-                            'DisplayName': permissions[folderID][2],
-                            'ID': permissions[folderID][3]
-                            },
-                        'Permission': 'READ'
-                    },
-                ]
-            }
-        };
-
-        // get reference to S3 client 
-        var s3 = new AWS.S3();
-
-        // Put the ACL on the object
-        s3.putObjectAcl(params, function(err, data) {
-            if (err) console.log(err, err.stack); // an error occurred
-            else     console.log(data);           // successful response
-        });
-    }
- };
+                  // get reference to S3 client 
+                  var s3 = new AWS.S3();
+                  // Put the ACL on the object
+                  s3.putObjectAcl(params, function(err, data) {
+                      if (err) console.log(err, err.stack); // an error occurred
+                      else     console.log(data);           // successful response
+                  });
+              }
+           };
